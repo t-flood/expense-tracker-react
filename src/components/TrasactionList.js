28c0009267af4ef83db6129233ddca4408ad3a2a
Tim Flood <tflood9@gmail.com>
2021-06-29 14:08:46 -0400

    created components and began work on context

---
+++ src/components/TrasactionList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TrasactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        <li className="minus">Cash</li>
+      </ul>
+    </>
+  );
+};
+
+export default TrasactionList;
